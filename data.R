3b60f8e9187856e661b6ff564a06c8b23fb11e96
tywerdel <ty.werdel@ag.tamu.edu>
2023-01-04 10:19:30 -0600

    Added a course

--- data.R
+++ data.R
@@ -109,7 +109,8 @@ reviewer <- tribble(
 
 teach <- tribble(
   ~area, ~accomplishment, ~year, ~where, ~detail,
-  "Texas A&M University", "Co-Instructor", "2022", "College Station, TX", "Techniques of Wildlife Management, RWFM 408 & WFSC 638",
+  "Texas A&M University", "Co-Instructor", "2022 -- 2023", "College Station, TX", "Special Topics in Traditional Ecological Knowledge, RWFM 689",
+  "Texas A&M University", "Co-Instructor", "2022 -- 2023", "College Station, TX", "Techniques of Wildlife Management, RWFM 408 & WFSC 638",
   "Kansas State University", "Graduate Teaching Assistant", "2017 -- 2022", "Manhattan, KS", "Firearms Cartridges and Ballistics, WOEM 201",
   "Kansas State University", "Graduate Teaching Assistant", "2017 -- 2022", "Manhattan, KS", "Rifle and Handgun Range Design, Construction and Operations, WOEM 202",
   "Kansas State University", "Graduate Teaching Assistant", "2017 -- 2022", "Manhattan, KS", "Introduction to Wildlife and Outdoor Enterprise Management, WOEM 250",
